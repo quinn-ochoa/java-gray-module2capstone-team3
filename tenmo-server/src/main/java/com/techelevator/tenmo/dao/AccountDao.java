44bc2ec9dbd2bfe5259d6ad9529529f81f8ef698
Quynh Ochoa <quynh.ochoa@gmail.com>
2024-03-06 08:58:00 -0800

    created Account DAO

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -1,4 +1,8 @@
 package com.techelevator.tenmo.dao;
 
+import java.math.BigDecimal;
+
 public interface AccountDao {
+    public BigDecimal getBalanceById(int id);
+    public BigDecimal updateBalanceById(int id);
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,4 +1,47 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.exception.DaoException;
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
 public class JdbcAccountDao implements AccountDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public BigDecimal getBalanceById(int id) {
+        BigDecimal balance = null;
+        String sql = "SELECT balance FROM account WHERE account_id = ?;";
+        try{
+            balance = jdbcTemplate.queryForObject(sql, BigDecimal.class, id);
+
+        } catch(CannotGetJdbcConnectionException e){
+            throw new DaoException("Unable to connect to server or database", e);
+        }
+        return balance;
+    }
+
+    @Override
+    public BigDecimal updateBalanceById(int id) {
+        return null;
+    }
+
+    public Account mapRowToAccount(SqlRowSet rowSet) {
+        Account account = new Account();
+        account.setAccountId(rowSet.getInt("account_id"));
+        account.setUserId(rowSet.getInt("user_id"));
+        account.setBalance(rowSet.getBigDecimal("balance"));
+
+        return account;
+    }
+
 }
